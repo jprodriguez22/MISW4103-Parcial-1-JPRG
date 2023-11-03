04b587be95f286dd9df9b0e25b9e6c6397cd9b28
Juan Rodriguez <jp.rodriguez22@uniandes.edu.co>
2023-11-03 18:19:47 -0500

    Crear servicio para Cafe

---
+++ el-aroma-magico/src/app/Cafe/Cafe.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Cafe } from './Cafe';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CafeService {
+    private apiURL: string = environment.baseUrl
+    
+    constructor(private http: HttpClient) { }
+
+    getCafes(): Observable<Cafe[]>{
+        return this.http.get<Cafe[]>(this.apiURL)
+    }
+}
+
