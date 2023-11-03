8bc99ecc633ed85293abf03db93787dbc904de79
Juan Rodriguez <jp.rodriguez22@uniandes.edu.co>
2023-11-03 17:45:00 -0500

    Crear proyecto y módulo Cafe

---
+++ el-aroma-magico/src/app/Cafe/Cafe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Cafe',
+  templateUrl: './Cafe.component.html',
+  styleUrls: ['./Cafe.component.css']
+})
+export class CafeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ el-aroma-magico/src/app/Cafe/Cafe.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CafeComponent } from './Cafe.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CafeComponent]
+})
+export class CafeModule { }
