d3a329bc7353167943be34b8324094a369f8ed59
Juan Rodriguez <jp.rodriguez22@uniandes.edu.co>
2023-11-03 22:40:51 -0500

    Visuales finalizadas

--- el-aroma-magico/src/app/Cafe/Cafe-list/Cafe-list.component.ts
+++ el-aroma-magico/src/app/Cafe/Cafe-list/Cafe-list.component.ts
@@ -10,15 +10,21 @@ import { CafeService } from '../Cafe.service';
 })
 export class CafeListComponent implements OnInit {
   cafes: Array<Cafe> = [];
+  cafesOrigen: number = 0;
+  cafesBlend: number = 0;
 
   constructor(private cafeService: CafeService) { }
 
   getCafes(): void {
+    const tipoBlend = "Blend";
+    const tipoOrigen = "Café de Origen"
     this.cafeService.getCafes().subscribe((cafes) => {
       this.cafes = cafes;
+      this.cafesBlend = this.cafes.reduce((a, c) =>  c.tipo === tipoBlend ? ++a: a,0);
+      this.cafesOrigen = this.cafes.reduce((a, c) =>  c.tipo === tipoOrigen ? ++a: a,0);
     })
   }
-
+ 
   ngOnInit(): void {
     this.getCafes();
   }
